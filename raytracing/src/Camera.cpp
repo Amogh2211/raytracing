1c73d0a988f8fe730a14966061a31ce4222aba85
Amogh2211 <amogh2211@gmail.com>
2023-10-02 22:11:23 +0530

    Path tracing for accumulated frames

--- raytracing/src/Camera.cpp
+++ raytracing/src/Camera.cpp
@@ -13,14 +13,14 @@ Camera::Camera(float verticalFOV, float nearClip, float farClip) : m_VerticalFOV
 	m_Position = glm::vec3(0, 0, 0);
 }
 
-void Camera::OnUpdate(float ts) {
+bool Camera::OnUpdate(float ts) {
 	glm::vec2 mousePos = Input::GetMousePosition();
 	glm::vec2 delta = (mousePos - m_LastMousePosition) * 0.002f;
 	m_LastMousePosition = mousePos;
 
 	if (!Input::IsMouseButtonDown(MouseButton::Right)) {
 		Input::SetCursorMode(CursorMode::Normal);
-			return;
+			return false;
 	}
 
 	Input::SetCursorMode(CursorMode::Locked);
@@ -77,6 +77,8 @@ void Camera::OnUpdate(float ts) {
 		RecalculateView();
 		RecalculateRayDirections();
 	}
+
+	return moved;
 }
 
 void Camera::OnResize(uint32_t width, uint32_t height) {
--- raytracing/src/Renderer.h
+++ raytracing/src/Renderer.h
@@ -8,6 +8,10 @@
 #include "Scene.h"
 
 class Renderer {
+public:
+	struct Settings {
+		bool Accumulate = true; // To check if accumulation is happening or no
+	};
 
 public:
 	Renderer() = default;
@@ -16,6 +20,9 @@ public:
 	void Render(const Scene& scene, const Camera& camera); 
 
 	std::shared_ptr<Walnut::Image> GetFinalImage() const { return m_FinalImage; };
+
+	void ResetFrameIndex() { m_FrameIndex = 1; };
+	Settings& GetSettings() { return m_Settings; };
 private:
 	struct HitPayload
 	{
@@ -33,9 +40,12 @@ private:
 	
 private:
 	std::shared_ptr<Walnut::Image> m_FinalImage;
-
+	Settings m_Settings;
 	const Scene* m_ActiveScene = nullptr;
 	const Camera* m_ActiveCamera = nullptr;
 
 	uint32_t* m_ImageData = nullptr;
+	glm::vec4* m_AccumulationData = nullptr;
+
+	uint32_t m_FrameIndex = 1; // To define an average on the accumulation
 };
