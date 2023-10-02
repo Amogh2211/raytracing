3cdd259f20aa6920d0c8abe6f5b167c1cc2ed044
Amogh2211 <amogh2211@gmail.com>
2023-10-02 20:45:40 +0530

    Added physically based rendering, refactored materials and indexed objects

--- raytracing/src/Scene.h
+++ raytracing/src/Scene.h
@@ -2,13 +2,22 @@
 #include <glm/glm.hpp>
 #include <vector>
 
+struct Material {
+
+	glm::vec3 Albedo{1.0f}; // Non lit diffuse component of a material
+	float Roughness = 1.0f;
+	float Metallic = 0.0f;
+
+};
+
 struct Sphere {
 	glm::vec3 Position{0.0f};
 	float Radius = 0.5f;
 
-	glm::vec3 Albedo{1.0f}; // Non lit diffuse component of a material
+	int MaterialIndex = 0;
 };
 
 struct Scene {
+	std::vector<Material> Materials;
 	std::vector<Sphere> Spheres;
 };
